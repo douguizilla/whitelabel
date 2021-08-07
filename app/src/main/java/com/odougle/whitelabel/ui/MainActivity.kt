233d41bf4825b9dbb4c9856394930c523c0c9db1
douguizilla <douglasgommez@gmail.com>
2021-08-07 11:55:20 -0300

    refactor MainActivity.kt to ui package

--- app/src/main/java/com/odougle/whitelabel/ui/MainActivity.kt
+++ app/src/main/java/com/odougle/whitelabel/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.odougle.whitelabel
+package com.odougle.whitelabel.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.odougle.whitelabel.R
 import com.odougle.whitelabel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
