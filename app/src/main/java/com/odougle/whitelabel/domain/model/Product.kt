504edec02cfcff910b2a096f65ad12f97f464b87
douguizilla <douglasgommez@gmail.com>
2021-08-10 10:46:26 -0300

    create project architecture and create data class Product.kt into model

---
+++ app/src/main/java/com/odougle/whitelabel/domain/model/Product.kt
@@ -0,0 +1,8 @@
+package com.odougle.whitelabel.domain.model
+
+data class Product(
+    val id: String = "",
+    val description: String = "",
+    val price: Double = 0.0,
+    var imageUrl: String  = ""
+)
