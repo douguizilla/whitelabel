2f7e8254475fe8ce0b899e3af27d1060b9f1cb62
douguizilla <douglasgommez@gmail.com>
2021-08-12 18:48:18 -0300

    inserted AddProductFragment.kt on nav_main.xml

--- app/src/main/java/com/odougle/whitelabel/ui/products/addproduct/AddProductFragment.kt
+++ app/src/main/java/com/odougle/whitelabel/ui/products/addproduct/AddProductFragment.kt
@@ -1,13 +1,13 @@
 package com.odougle.whitelabel.ui.products.addproduct
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.odougle.whitelabel.databinding.AddProductFragmentBinding
 
-class AddProductFragment : Fragment() {
+class AddProductFragment : BottomSheetDialogFragment() {
 
     private var _binding: AddProductFragmentBinding? = null
     private val binding get() = _binding!!
