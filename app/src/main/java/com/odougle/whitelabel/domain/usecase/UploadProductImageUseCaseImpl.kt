1e9e5a091f3b3156d7e9c9048f614b8545b9ad9e
douguizilla <douglasgommez@gmail.com>
2021-08-12 17:53:14 -0300

    created implementation class to UploadProductImageUseCase.kt

---
+++ app/src/main/java/com/odougle/whitelabel/domain/usecase/UploadProductImageUseCaseImpl.kt
@@ -0,0 +1,9 @@
+package com.odougle.whitelabel.domain.usecase
+
+import android.net.Uri
+
+class UploadProductImageUseCaseImpl : UploadProductImageUseCase {
+    override suspend fun invoke(imageUri: Uri): String {
+        TODO("Not yet implemented")
+    }
+}
