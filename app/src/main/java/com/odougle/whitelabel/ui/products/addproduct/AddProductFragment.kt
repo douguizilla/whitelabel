b7a58f9e3e32324c5c8b7a1b2af917ec792c5cd7
douguizilla <douglasgommez@gmail.com>
2021-08-12 19:10:56 -0300

    implement listeners for image product insertion, add button click and formatting price when it's writting by the user

--- app/src/main/java/com/odougle/whitelabel/ui/products/addproduct/AddProductFragment.kt
+++ app/src/main/java/com/odougle/whitelabel/ui/products/addproduct/AddProductFragment.kt
@@ -1,16 +1,28 @@
 package com.odougle.whitelabel.ui.products.addproduct
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.odougle.whitelabel.databinding.AddProductFragmentBinding
+import com.odougle.whitelabel.util.CurrencyTextWatcher
 
 class AddProductFragment : BottomSheetDialogFragment() {
 
     private var _binding: AddProductFragmentBinding? = null
     private val binding get() = _binding!!
+
+    private var imageUri: Uri? = null
+
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
+            imageUri = uri
+            binding.imageProduct.setImageURI(uri)
+        }
 
     private lateinit var viewModel: AddProductViewModel
 
@@ -20,6 +32,25 @@ class AddProductFragment : BottomSheetDialogFragment() {
     ): View? {
         _binding = AddProductFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun setListeners(){
+        binding.imageProduct.setOnClickListener {
+            chooseImage()
+        }
+
+        binding.buttonAddProduct.setOnClickListener {
+            val description = binding.inputDescription.text.toString()
+            val price = binding.inputPrice.text.toString()
+        }
+
+        binding.inputPrice.run {
+            addTextChangedListener(CurrencyTextWatcher(this))
+        }
+    }
+
+    private fun chooseImage(){
+        getContent.launch("image/*")
     }
 
 
