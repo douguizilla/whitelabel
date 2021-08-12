4200465671d388583092f77ce78ead0a15e2673d
douguizilla <douglasgommez@gmail.com>
2021-08-12 18:27:21 -0300

    optimize imports

--- app/src/main/java/com/odougle/whitelabel/ui/products/addproduct/AddProductFragment.kt
+++ app/src/main/java/com/odougle/whitelabel/ui/products/addproduct/AddProductFragment.kt
@@ -1,12 +1,10 @@
 package com.odougle.whitelabel.ui.products.addproduct
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.odougle.whitelabel.R
 import com.odougle.whitelabel.databinding.AddProductFragmentBinding
 
 class AddProductFragment : Fragment() {
