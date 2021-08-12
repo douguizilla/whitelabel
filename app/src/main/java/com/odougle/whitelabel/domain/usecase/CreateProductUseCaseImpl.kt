2d50d7dd28ada00cfc55ecf52575fe9a4eaaecbd
douguizilla <douglasgommez@gmail.com>
2021-08-12 18:06:57 -0300

    implemented invoke function to handle the product creation

--- app/src/main/java/com/odougle/whitelabel/domain/usecase/CreateProductUseCaseImpl.kt
+++ app/src/main/java/com/odougle/whitelabel/domain/usecase/CreateProductUseCaseImpl.kt
@@ -4,13 +4,19 @@ import android.net.Uri
 import com.odougle.whitelabel.data.ProductRepository
 import com.odougle.whitelabel.domain.model.Product
 import java.lang.Exception
+import java.util.*
 
 class CreateProductUseCaseImpl(
     private val uploadProductImageUseCase: UploadProductImageUseCase,
     private val productRepository: ProductRepository
 ) : CreateProductUseCase {
     override suspend fun invoke(description: String, price: Double, imageUri: Uri): Product {
-
+        return try {
+            val imageUrl = uploadProductImageUseCase(imageUri)
+            val product = Product(UUID.randomUUID().toString(), description, price, imageUrl)
+            productRepository.createProduct(product)
+        } catch (e: Exception) {
+            throw e
+        }
     }
-
 }
