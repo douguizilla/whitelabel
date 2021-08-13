447f2d289de8c7c18a2141dc6f503344f3be1378
douguizilla <douglasgommez@gmail.com>
2021-08-13 14:45:48 -0300

    start to implement AddProductViewModel.kt passing CreateProductUseCase interface as a parameter

--- app/src/main/java/com/odougle/whitelabel/ui/products/addproduct/AddProductViewModel.kt
+++ app/src/main/java/com/odougle/whitelabel/ui/products/addproduct/AddProductViewModel.kt
@@ -1,7 +1,14 @@
 package com.odougle.whitelabel.ui.products.addproduct
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.odougle.whitelabel.domain.usecase.CreateProductUseCase
+import kotlinx.coroutines.launch
 
-class AddProductViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AddProductViewModel(
+    private val createProductUseCase: CreateProductUseCase
+) : ViewModel() {
+
+
 }
