c19c20cbefd0b844554093c8c4a0868f2598047d
douguizilla <douglasgommez@gmail.com>
2021-08-14 16:03:33 -0300

    set dependency injection

--- app/src/main/java/com/odougle/whitelabel/ui/products/ProductsViewModel.kt
+++ app/src/main/java/com/odougle/whitelabel/ui/products/ProductsViewModel.kt
@@ -1,7 +1,13 @@
 package com.odougle.whitelabel.ui.products
 
 import androidx.lifecycle.ViewModel
+import com.odougle.whitelabel.domain.usecase.GetProductsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ProductsViewModel : ViewModel() {
+@HiltViewModel
+class ProductsViewModel @Inject constructor(
+    private val getProductsUseCase: GetProductsUseCase
+) : ViewModel() {
 
 }
