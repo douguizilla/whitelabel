ac24df05e14495ee32fc15edcb53fd2abf99abc4
douguizilla <douglasgommez@gmail.com>
2021-08-14 16:06:32 -0300

    implemented getProducts on view model

--- app/src/main/java/com/odougle/whitelabel/ui/products/ProductsViewModel.kt
+++ app/src/main/java/com/odougle/whitelabel/ui/products/ProductsViewModel.kt
@@ -1,13 +1,31 @@
 package com.odougle.whitelabel.ui.products
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.odougle.whitelabel.domain.model.Product
 import com.odougle.whitelabel.domain.usecase.GetProductsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.Exception
 
 @HiltViewModel
 class ProductsViewModel @Inject constructor(
     private val getProductsUseCase: GetProductsUseCase
 ) : ViewModel() {
 
+    private val _productData = MutableLiveData<List<Product>>()
+    val productsData: LiveData<List<Product>> = _productData
+
+    fun getProducts() = viewModelScope.launch {
+        try {
+            val products = getProductsUseCase()
+            _productData.value = products
+        }catch (e: Exception){
+            Log.d("ProductsViewModel", e.toString())
+        }
+    }
 }
