35986cf7ef3d898b1e13582f91288eaf120a2598
douguizilla <douglasgommez@gmail.com>
2021-08-14 16:08:09 -0300

    put viewModel and set fragment as Android Entry Point

--- app/src/main/java/com/odougle/whitelabel/ui/products/ProductsFragment.kt
+++ app/src/main/java/com/odougle/whitelabel/ui/products/ProductsFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.odougle.whitelabel.databinding.FragmentProductsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProductsFragment : Fragment() {
 
     private var _binding: FragmentProductsBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: ProductsViewModel by viewModels()
 
     private val productsAdapter = ProductsAdapter()
 
