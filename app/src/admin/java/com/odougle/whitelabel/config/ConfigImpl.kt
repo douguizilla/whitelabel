3f088bc3f0748239389753f782f24443248b993b
douguizilla <douglasgommez@gmail.com>
2021-08-15 13:01:02 -0300

    created live data to addButtonVisibility

--- app/src/admin/java/com/odougle/whitelabel/config/ConfigImpl.kt
+++ app/src/admin/java/com/odougle/whitelabel/config/ConfigImpl.kt
@@ -1,7 +1,8 @@
 package com.odougle.whitelabel.config
 
+import android.view.View
 import javax.inject.Inject
 
 class ConfigImpl @Inject constructor() : Config {
-    override val addButtonVisibility: Boolean = true
+    override val addButtonVisibility: Int = View.VISIBLE
 }
